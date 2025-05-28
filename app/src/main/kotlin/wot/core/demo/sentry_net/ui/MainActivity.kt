45e92b6aa403d31181263ba6e1c07f8e51684212
yangsn <ysn_2020@163.com>
2025-05-28 13:54:35 +0800

    [ +] 将模板库初始化为网络监控库

---
+++ app/src/main/kotlin/wot/core/demo/sentry_net/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package wot.core.demo.sentry_net.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import wot.core.demo.sentry_net.R
+
+/**
+ * 功能说明
+ *
+ * @author : yangsn
+ * @date : 2025/5/27
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
---
+++ sentry_net/src/main/kotlin/wot/core/net/sentry_net/Template.kt
@@ -0,0 +1,10 @@
+package wot.core.net.sentry_net
+
+/**
+ * 功能说明
+ *
+ * @author : yangsn
+ * @date : 2025/5/28
+ */
+class Template {
+}
